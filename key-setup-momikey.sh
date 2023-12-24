cf31d3abd7d2456718962d78f55644a4092623a1
wata2ki <wata2ki@gmail.com>
2023-12-25 01:03:19 +0900

    Update key setting script
    Signed-off-by: wata2ki <wata2ki@gmail.com>

--- key-setup-momikey.sh
+++ key-setup-momikey.sh
@@ -11,17 +11,17 @@ echo "| 9 | 6 | 3 |  |16,17,18|"
 echo "-------------   -------- "
 echo ""
 
-./usb-12key-kbd-prog 1 1 n
-./usb-12key-kbd-prog 1 2 n
-./usb-12key-kbd-prog 1 3 c
-./usb-12key-kbd-prog 1 4 h
-./usb-12key-kbd-prog 1 5 n
-./usb-12key-kbd-prog 1 6 n
+./usb-12key-kbd-prog 1 1 d
+./usb-12key-kbd-prog 1 2 b
+./usb-12key-kbd-prog 1 3 F3
+./usb-12key-kbd-prog 1 4 s
+./usb-12key-kbd-prog 1 5 a
+./usb-12key-kbd-prog 1 6 F2
 ./usb-12key-kbd-prog 1 7 g
-./usb-12key-kbd-prog 1 8 d
-./usb-12key-kbd-prog 1 9 a
+./usb-12key-kbd-prog 1 8 h
+./usb-12key-kbd-prog 1 9 F1
 ./usb-12key-kbd-prog 1 13 n
-./usb-12key-kbd-prog 1 14 n
+./usb-12key-kbd-prog 1 14 c
 ./usb-12key-kbd-prog 1 15 n
 ./usb-12key-kbd-prog 1 16 n
 ./usb-12key-kbd-prog 1 17 n
@@ -29,15 +29,20 @@ echo ""
 
 echo "AGL momikey setting"
 echo "-------------   -------- "
-echo "| g | h | n |  | n, n, n|"
+echo "| g | s | d |  | n, c, n|"
 echo "-------------   -------- "
-echo "| d | n | 2 |            "
+echo "| h | a | b |            "
 echo "-------------   -------- "
-echo "| a | n | c |  | n, n, n|"
+echo "| F1| F2| F3|  | n, n, n|"
 echo "-------------   -------- "
-echo "g : agl-flutter-ivi-demo"
-echo "d : agl-qt-ivi-demo"
-echo "a : agl-momi-ivi-demo"
-echo "h : agl-html5-ivi-demo"
+echo "g : AGL Flutter demo IVI CES2024"
+echo "s : AGL Flutter demo IVI CES2023"
+echo "d : AGL Qt demo IVI"
+echo "h : AGL HTML5 demo IVI"
+echo "a : AGL Momi demo IVI"
+echo "b : AGL Safe Momi demo IVI"
 echo "c : force reboot guest"
+echo "F1: Teltail 1"
+echo "F2: Teltail 2"
+echo "F3: Teltail 3"
 echo ""
